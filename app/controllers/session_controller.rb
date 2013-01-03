6c803ae57f56d2ce22fa92b92c1121c62363be14
Tim LaTorre <timlatorre@gmail.com>
2013-01-03 17:05:00 -0500

    logout and nav bar

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
     user = User.where(:email => params[:email]).first
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to overview_path
+      redirect_to root_path
     else
       render :new
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
 
   def display_links
     if @auth
+      link_to("Home", root_path) +
+      link_to("Logout", logout_path, :method => :delete)
     else
       link_to("Login", login_path) +
       link_to("Register", register_path)
