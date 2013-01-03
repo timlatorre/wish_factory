5732b4a4c74874854db319991ead9c1b864e3a1f
Tim LaTorre <timlatorre@gmail.com>
2013-01-03 13:37:31 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishFactory::Application.config.secret_token = '9231a214ffdfb7c9db2721ec2054b79a3921ab3b247a24629a67237751cf9db2bca88baaa19f39ff491bb06ecd66648168facf42f2add12684a94ba28bbf3d9f'
